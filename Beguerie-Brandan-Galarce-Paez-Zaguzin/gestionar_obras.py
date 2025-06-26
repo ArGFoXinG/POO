fe2fdcacd8ffacebdb1fc0729f2c57034df454a6
MartinianoGalarce <nanogalarce@gmail.com>
2025-06-26 12:39:17 -0300

    utf 8

--- Beguerie-Brandan-Galarce-Paez-Zaguzin/gestionar_obras.py
+++ Beguerie-Brandan-Galarce-Paez-Zaguzin/gestionar_obras.py
@@ -40,13 +40,13 @@ class GestionarObra:
         # Extrae datos del archivo CSV.
 
         try:
-            with open('observatorio-de-obras-urbanas.csv', 'r', encoding='latin-1') as f:
+            with open('observatorio-de-obras-urbanas.csv', 'r', encoding='UTF-8') as f:
                 primera_linea = f.readline()
                 print("Encabezados del CSV (crudos):", primera_linea.strip())
 
             df = pd.read_csv(
                 'observatorio-de-obras-urbanas.csv',
-                encoding='latin-1',
+                encoding='UTF-8',
                 delimiter=';',
                 on_bad_lines='skip'
             )
