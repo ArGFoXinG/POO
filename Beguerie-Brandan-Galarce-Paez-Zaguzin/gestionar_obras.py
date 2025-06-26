7d96b305353079c418a7c45138fbc13a87449d03
tovzag <xzaguzin@gmail.com>
2025-06-26 09:45:49 -0300

    debug

--- Beguerie-Brandan-Galarce-Paez-Zaguzin/gestionar_obras.py
+++ Beguerie-Brandan-Galarce-Paez-Zaguzin/gestionar_obras.py
@@ -40,7 +40,7 @@ class GestionarObra:
         # Extrae datos del archivo CSV.
 
         try:
-            with open('observatorio-de-obras-urbanas.csv', 'r', encoding='UTF-8') as f:
+            with open('observatorio-de-obras-urbanas.csv', 'r', encoding='latin-1') as f:
                 primera_linea = f.readline()
                 print("Encabezados del CSV (crudos):", primera_linea.strip())
 
